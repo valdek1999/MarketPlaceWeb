0c5c2ca4f0bc55b874ea85f151fe7bcd7621a896
IrkhiII <IrkhiII@ugsk.ru>
2022-08-22 22:50:50 +0300

    Add new controller Products.  Change gitignore

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlaceWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Product> Get(int sheet)
+        {
+            var products = new List<Product>();
+            products.AddRange(new[] {
+                new Product { Id = 1, Name = "Pepa", Description = "Some first discription", Price = 5.5f },
+                new Product() { Id = 2, Name = "Pepa", Description = "Some second discription", Price = 9.5f }});
+            return products;
+        }
+    }
+}
