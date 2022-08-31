cc88ae0aba0123a05b5cf88338f54c5665bb7a2a
Gosha94 <gerizch@rambler.ru>
2022-08-31 23:37:02 +0300

    change folders

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -24,7 +24,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: '', component: HomeComponent, pathMatch: 'full', redirectTo: '/home' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ]),
